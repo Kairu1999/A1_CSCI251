1bfc9268f05b033023ad584cc1f8f31923cded27
Shane-Kyle Chua <90674209+Kairu1999@users.noreply.github.com>
2023-07-10 01:04:19 +0800

    Push more vectors!

--- A1_CSCI251/readFile.cpp
+++ A1_CSCI251/readFile.cpp
@@ -2,8 +2,14 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <sstream>
 
 using namespace std;
+
+//function prototype:
+
+void readFromFile(string filename);
+vector<string> splitString(string input, string delimiter);
 
 void readFromFile(string filename)
 {   
@@ -23,16 +29,50 @@ void readFromFile(string filename)
             //declare string line variable
             string line;
 
+            stringstream ss{};
+
+
             //store file data in variable
             getline(inputfile, line);
 
-            //print to debug     
-            cout << line << endl;
-
             //declare a vector
-            std::vector<string> vect1{};
+            vector<string> vect1{};
 
-            //use 'new" to store map data in assignment vect1
+            //Used for X and Y Coordinates
+            vector<vector<int>> Coordinates{};
+
+            //store the city type
+            vector<int> City_Area{};
+
+            //store the last perimeter
+            vector<string> City_Type{};
+
+            //split the string
+            vect1 = splitString(line, "-");
+            
+            //every 3 = coordinates so += 3, Do for Coordinates
+            for (int i = 0; i < vect1.size(); i += 3)
+            {
+                //cout << vect1[i] << endl;
+                //Need to find a way to remove the "[ ]" and the ","
+            }
+
+            //every 3 = coordinates so += 3, do the same for city Area!
+            for (int j = 1; j < vect1.size(); j+=3) 
+            {
+                //cout << vect1[j] << endl;
+
+                //string to integer conversion
+                City_Area.push_back(stoi(vect1[j]));
+            }
+
+            //every 3 = coordinates so += 3, do the same for city Type!
+            for (int k = 2; k < vect1.size(); k += 3)
+            {
+                //cout << vect1[k] << endl;
+                City_Type.push_back(vect1[k]);
+            }
+
         }
     }
     else {
