afa4c001a9e48b0da1ed1b8c873d9d0b949747b0
Shane-Kyle CHUA <shanekyle.chua@digipen.edu>
2023-07-21 14:51:30 +0800

    ALL GRIDS DONE

--- A1_CSCI251/grid.cpp
+++ A1_CSCI251/grid.cpp
@@ -7,10 +7,6 @@ void generate_grid_city(int** coordinates, int xsize, int ysize)
 {
 	cout << "Generating grid with numbers!" << endl;
 
-	////THIS IS FOR THE BOX
-	//xsize += 2;
-	//ysize += 2;
-
 	//THIS IS THE TOP WALL
 	for (int i = 0; i <= ysize+2; ++i)
 	{
@@ -59,7 +55,10 @@ void generate_grid_city(int** coordinates, int xsize, int ysize)
 	}
 
 
-	for (int j = 0; j < 2; j++) {
+	//This is for the bottom wall
+	for (int j = 0; j < 2; j++) 
+	{
+
 		for (int i = 0; i <= ysize; ++i)
 		{
 			if (j == 0)
@@ -96,6 +95,89 @@ void generate_grid_city(int** coordinates, int xsize, int ysize)
 void generate_grid(int** coordinates, int xsize, int ysize)
 {
 	cout << "Generating grid with numbers!" << endl;
+	//THIS IS THE TOP WALL
+	for (int i = 0; i <= ysize + 2; ++i)
+	{
+		if (i == 0)
+		{
+			cout << " #";
+		}
+		else
+		{
+			cout << "#";
+		}
+	}
+	cout << endl;
+
+	//THIS IS THE DATA
+	for (int i = 0; i <= ysize; ++i)
+	{
+		for (int j = 0; j <= xsize; ++j)
+		{
+			//for the left wall
+			if (j == 0)
+			{
+				//i value + wall of #
+				cout << (ysize - i) << "#";
+			}
+
+			//print coordinates whether wall was printed or not
+			if (coordinates[i][j] == -1)
+			{
+				//print the rest of the grid
+				cout << " ";
+			}
+			else
+			{
+				cout << coordinates[i][j];
+			}
+
+			//print back wall
+			if (j == xsize)
+			{
+				//print the back wall of the grid
+				cout << "#";
+			}
+		}
+		cout << endl;
+	}
+
+
+	//This is for the bottom wall
+	for (int j = 0; j < 2; j++)
+	{
+
+		for (int i = 0; i <= ysize; ++i)
+		{
+			if (j == 0)
+			{
+				if (i == 0)
+				{
+					cout << " #";
+				}
+				else if (i == ysize)
+				{
+					cout << "###";
+				}
+				else
+				{
+					cout << "#";
+				}
+			}
+			if (j == 1)
+			{
+				if (i == 0)
+				{
+					cout << "  " << i;
+				}
+				else
+				{
+					cout << i;
+				}
+			}
+		}
+		cout << endl;
+	}
 
 
 }
@@ -104,35 +186,86 @@ void generate_gridLMH(int** coordinates, int xsize, int ysize)
 {
 	cout << "Generating grid with LMH Values: " << endl;
 	cout << endl;
-
-	////THIS IS FOR THE BOX
-	//for (int row = 0; row < xsize; row++)
-	//{
-	//	for (int col = 0; col < ysize; col++)
-	//	{
-	//		//for up and down lines
-	//		if ((row == 0) || (row = xsize - 1)) 
-	//		{
-	//			cout << "# ";
-	//		}
-
-	//		//for left and right lines
-	//		else if ((col == 0) || (col = ysize - 1)) {
-	//			cout << "#";
-	//		}
-	//		else {
-	//			cout << " ";
-	//		}
-	//	}
-	//	cout << endl;
-	//}
-
-//TEST PRINTED VALUES
-	for (int i = 0; i <= xsize; ++i)
+	//THIS IS THE TOP WALL
+	for (int i = 0; i <= ysize + 2; ++i)
 	{
-		for (int j = 0; j <= ysize; ++j)
+		if (i == 0)
 		{
-			cout << static_cast<char>(coordinates[i][j]) << " ";
+			cout << " #";
+		}
+		else
+		{
+			cout << "#";
+		}
+	}
+	cout << endl;
+
+	//THIS IS THE DATA
+	for (int i = 0; i <= ysize; ++i)
+	{
+		for (int j = 0; j <= xsize; ++j)
+		{
+			//for the left wall
+			if (j == 0)
+			{
+				//i value + wall of #
+				cout << (ysize - i) << "#";
+			}
+
+			//print coordinates whether wall was printed or not
+			if (coordinates[i][j] == -1)
+			{
+				//print the rest of the grid
+				cout << " ";
+			}
+			else
+			{
+				cout << static_cast<char>(coordinates[i][j]);
+			}
+
+			//print back wall
+			if (j == xsize)
+			{
+				//print the back wall of the grid
+				cout << "#";
+			}
+		}
+		cout << endl;
+	}
+
+
+	//This is for the bottom wall
+	for (int j = 0; j < 2; j++)
+	{
+
+		for (int i = 0; i <= ysize; ++i)
+		{
+			if (j == 0)
+			{
+				if (i == 0)
+				{
+					cout << " #";
+				}
+				else if (i == ysize)
+				{
+					cout << "###";
+				}
+				else
+				{
+					cout << "#";
+				}
+			}
+			if (j == 1)
+			{
+				if (i == 0)
+				{
+					cout << "  " << i;
+				}
+				else
+				{
+					cout << i;
+				}
+			}
 		}
 		cout << endl;
 	}
