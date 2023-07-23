8d41e70e668d3740140787ca09a4556aeacaa3f9
Shane-Kyle Chua <90674209+Kairu1999@users.noreply.github.com>
2023-07-23 14:06:24 +0800

    ASCII Rain Class done!

--- A1_CSCI251/WeatherReport.cpp
+++ A1_CSCI251/WeatherReport.cpp
@@ -7,14 +7,19 @@
 #include "Structs.h";
 using namespace std;
 
+
 void print_weather_report(vector<cityStructure> city, vector<cloudyPressure> cloudy, vector<cloudyPressure> pressure, int xsize, int ysize)
 {
 	cout << "Weather Forecast Summary Report" << endl;
 	cout << "-------------------------------" << endl;
 
 	vector<int> cityType{};
+
 	vector<cityStructure> cityInfo{};
 	vector<cityStructure> Temp{};
+
+	//to use for display:
+	vector<cityStructure> displayVect{};
 
 	for (int i = 0; i < city.size(); ++i) 
 	{
@@ -45,11 +50,13 @@ void print_weather_report(vector<cityStructure> city, vector<cloudyPressure> clo
 		}
 	}
 
-	//for debug
-	for (int k = 0; k < Temp.size(); ++k)
+	//populate the display vector
+	for (int l = 0; l < cityType.size(); ++l) 
 	{
-		cout << Temp[k].coordinates.x << " " << Temp[k].coordinates.y << " " << Temp[k].cityType << endl;
+		cout << cityType[l] << endl;
 	}
+
+	
 
 
 
@@ -74,4 +81,56 @@ vector<int> findUnique(vector<int> info)
 	//return result
 	return result;
 
+}
+
+string AsciiRainArt(char cloudCover, char Pressure, int rain) 
+{
+	string l{}, m{}, n{}, o{}, p{}, q{}, r{}, s{}, t{};
+
+	l = "~\n~~\n";
+	m = "~~\n~~~\n";
+	n = "~~~\n~~~~\n";
+	o = "~~~~\n~~~~~\n";
+	p = "~~~~\n~~~~~\n    \\";
+	q = "~~~~\n~~~~~\n   \\\\";
+	r = "~~~~\n~~~~~\n  \\\\\\";
+	s = "~~~~\n~~~~~\n \\\\\\\\";
+	t = "~~~~\n~~~~~\n\\\\\\\\\\";
+
+	if ((cloudCover == 'L') && (Pressure == 'H') && (rain == 90)) 
+	{
+		return t;
+	}
+	if ((cloudCover == 'L') && (Pressure == 'M') && (rain == 80))
+	{
+		return s;
+	}
+	if ((cloudCover == 'L') && (Pressure == 'L') && (rain == 70))
+	{
+		return r;
+	}
+	if ((cloudCover == 'M') && (Pressure == 'H') && (rain == 60))
+	{
+		return q;
+	}
+	if ((cloudCover == 'M') && (Pressure == 'M') && (rain == 50))
+	{
+		return p;
+	}
+	if ((cloudCover == 'M') && (Pressure == 'L') && (rain == 40))
+	{
+		return o;
+	}
+	if ((cloudCover == 'H') && (Pressure == 'H') && (rain == 30))
+	{
+		return n;
+	}
+	if ((cloudCover == 'H') && (Pressure == 'M') && (rain == 20))
+	{
+		return m;
+	}
+	if ((cloudCover == 'H') && (Pressure == 'L') && (rain == 10))
+	{
+		return l;
+	}
 }
--- A1_CSCI251/WeatherReport.h
+++ A1_CSCI251/WeatherReport.h
@@ -2,10 +2,23 @@
 #define WEATHEREPORT_H
 
 #include <vector>
+#include <string>
 #include "Structs.h"
 using namespace std;
 
+struct display 
+{
+	string cityTypeName;
+	int cityNumber;
+	string Average_cloudcover;
+	string Average_pressure;
+	float Rain_Probability;
+
+	//display ascii art
+	string ascii_rain;
+};
+
 void print_weather_report(vector<cityStructure> city, vector<cloudyPressure> cloudy, vector<cloudyPressure> pressure, int xsize, int ysize);
 vector<int>findUnique(vector<int> info);
-
+string AsciiRainArt(char cloudCover, char Pressure, int rain);
 #endif
