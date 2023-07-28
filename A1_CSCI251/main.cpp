97ff55b4aad6356c3f3f1887f8329fdd8cceb405
Shane-Kyle CHUA <shanekyle.chua@digipen.edu>
2023-07-28 14:36:17 +0800

    fixed grids

--- A1_CSCI251/main.cpp
+++ A1_CSCI251/main.cpp
@@ -101,16 +101,14 @@ int main()
                 cloudCoverVect = readCloudCoverFile(files[5]);
                 PressureVect = readPressureFile(files[6]);
 
-
-
-
                 //allocate memory and populate the array
                 CityStructure = Allocate2DArrayMemory_city(stoi(files[1]), stoi(files[3]), cityVect);
                 Cloudy = Allocate2DArrayMemory_cp(stoi(files[1]), stoi(files[3]),cloudCoverVect);
                 Pressure = Allocate2DArrayMemory_cp(stoi(files[1]), stoi(files[3]),PressureVect);
-                //Low Medium High
+                ////Low Medium High
                 CloudyLMH = Allocate2DArrayMemory_cp(stoi(files[1]), stoi(files[3]), cloudCoverVect);
                 PressureLMH = Allocate2DArrayMemory_cp(stoi(files[1]), stoi(files[3]), PressureVect);
+
 
 
                 //try printing output
@@ -125,7 +123,6 @@ int main()
                 Update2DArray(CloudyLMH, stoi(files[1]), stoi(files[3]), 1);
                 Update2DArray(Pressure, stoi(files[1]), stoi(files[3]),0);
                 Update2DArray(PressureLMH, stoi(files[1]), stoi(files[3]),1);
-
 
 
                 //print_2DArr_output_city(stoi(files[1]), stoi(files[3]), CityStructure);
@@ -231,11 +228,11 @@ int main()
             cout << "Exiting Program. Goodbye!" << endl;
             
             //Deallocate memory here!
-            DeAllocate2DArrayMemory(CityStructure, stoi(files[1]));
+           /* DeAllocate2DArrayMemory(CityStructure, stoi(files[1]));
             DeAllocate2DArrayMemory(Cloudy, stoi(files[1]));
             DeAllocate2DArrayMemory(Pressure, stoi(files[1]));
             DeAllocate2DArrayMemory(CloudyLMH, stoi(files[1]));
-            DeAllocate2DArrayMemory(PressureLMH, stoi(files[1]));
+            DeAllocate2DArrayMemory(PressureLMH, stoi(files[1]));*/
             return 0;
 
         default:
